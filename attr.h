81bcd206e769e04ba1e1e8d51a7a8031a72ee764
David Parsons <david.parsons@rutgers.edu>
2018-04-20 14:45:58 -0400

    Add Base Files for Proj3

---
+++ attr.h
@@ -0,0 +1,16 @@
+/**********************************************
+        CS415  Project 3
+        Spring  2018
+        Student Version
+**********************************************/
+
+#ifndef ATTR_H
+#define ATTR_H
+
+typedef union {int num; char *str;} tokentype;
+
+typedef struct {
+        int targetRegister;
+        } regInfo;
+
+#endif
---
+++ valnum.c
@@ -0,0 +1,11 @@
+/**********************************************
+        CS415  Project 3
+        Spring  2018
+        Student Version
+**********************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "instrutil.h"
+#include "valnum.h"
